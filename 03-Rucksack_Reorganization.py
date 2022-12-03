a8a0bf9510a5ea2eebec80dbb8bdc0589cf1f5fd
ckraasch <corinna@kraasch.de>
2022-12-03 15:14:17 +0100

    Solution for day 3.

---
+++ 03-Rucksack_Reorganization.py
@@ -0,0 +1,37 @@
+import string
+
+with open("Rucksack_Contents.txt", "r") as file:
+    my_list = file.read().splitlines()
+
+ltrs = list(string.ascii_letters)
+
+# Part 1
+lst1 = []
+
+for x1 in my_list:
+    for i1 in x1[:len(x1)//2]:
+        if i1 in x1[len(x1)//2:]:
+            lst1.append(ltrs.index(i1)+1)
+            break
+
+print(sum(lst1))
+
+# Part 2
+lst2 = []
+sublst2 = []
+n = 0
+
+for x2 in my_list:
+    sublst2.append(x2)
+    if n == 2:
+        for i2 in sublst2[0]:
+            if i2 in sublst2[1] and i2 in sublst2[2]:
+                lst2.append(ltrs.index(i2)+1)
+                break
+        n = 0
+        sublst2 = []
+    else:
+        n = n + 1
+
+print(sum(lst2))
+
