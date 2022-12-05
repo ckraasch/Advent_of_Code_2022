f145bc774eb5a9d9ee34c75e999bbb8b14e195c0
ckraasch <corinna@kraasch.de>
2022-12-05 12:37:30 +0100

    Solution for day 4.

---
+++ 04-Camp_Cleanup.py
@@ -0,0 +1,31 @@
+with open('Section_IDs.txt', 'r') as file:
+    all_sections = file.read().replace(",", "\n").replace("-", "\n").splitlines()   # Can I combine the two replace functions?
+
+pairs = len(all_sections)//4
+counter1 = 0
+counter2 = 0
+
+for each_pair in range(pairs):
+    n = int(each_pair)*4
+# Just to make the following better readable:
+    start1 = int(all_sections[n])
+    end1 = int(all_sections[n + 1])
+    start2 = int(all_sections[n + 2])
+    end2 = int(all_sections[n + 3])
+# Part 1
+    if start1 <= start2 and end1 >= end2:
+        counter1 += 1
+    elif start1 >= start2 and end1 <= end2:
+        counter1 += 1
+# Part 2
+    if end2 >= start1 >= start2:
+        counter2 += 1
+    elif end1 >= start2 >= start1:
+        counter2 += 1
+
+print("Part 1:", counter1)
+print("Part 2:", counter2)
+
+file.close()
+
+
