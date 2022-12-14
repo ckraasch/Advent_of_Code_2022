4423df36605b04545477269780e6661730e13812
ckraasch <corinna@kraasch.de>
2022-12-14 18:10:02 +0100

    Solution for day 10.

--- 10-Cathode_Ray_Tube.py
+++ 10-Cathode_Ray_Tube.py
@@ -3,30 +3,40 @@ with open("CPU_input-test2.txt", "r") as file:
 print(cpu_input)
 
 
+def cycling(cycle0, signal0, x0, n0, lst0, counter0):
+    counter0 += 1
+    if counter0 in range(x0-1, x0+1):
+        print(counter0, x0)
+        lst0[n0] += "#"
+    else:
+        lst0[n0] += "."
+    cycle0 += 1
+    if cycle0 == 20 or (cycle0 - 20) % 40 == 0:
+        signal0 += (cycle0 * x0)
+    if (cycle0-1) % 40 == 0:
+        n0 += 1
+        counter0 = 0
+    return cycle0, signal0, x0, n0, counter0
+
+
 counter = 0
-crt = []
-row = 0
+lst = [""]*6
+n = 0
 signal = 0
 cycle = 1
 x = 1
 for i in cpu_input:
-    if i == "noop":
-        cycle += 1
-        if cycle == 20 or (cycle-20) % 40 == 0:
-            signal += (cycle * x)
-    else:
-        cycle += 1
-        if cycle == 20 or (cycle-20) % 40 == 0:
-            signal += (cycle * x)
+    if i != "noop":
+        cycle, signal, x, n, counter = cycling(cycle, signal, x, n, lst, counter)
         x += int(i[5:])
-        cycle += 1
-        if cycle == 20 or (cycle-20) % 40 == 0:
-            signal += (cycle * x)
-    if counter in range(x - 1, x + 1):
-        crt.insert(counter, "#")
-    else:
-        crt.insert(counter, ".")
-    counter += 1
+    cycle, signal, x, n, counter = cycling(cycle, signal, x, n, lst, counter)
+
+
 print("Part 1:", signal)
 print("Part 2:")
-print(crt)
+print(lst[0])
+print(lst[1])
+print(lst[2])
+print(lst[3])
+print(lst[4])
+print(lst[5])
