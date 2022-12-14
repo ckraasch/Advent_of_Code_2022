f225f9cada41d38afc0b201a8c746e4b20c9c81e
ckraasch <corinna@kraasch.de>
2022-12-15 00:09:57 +0100

    Information for day 11.

--- 11-Monkey_in_the_Middle.py
+++ 11-Monkey_in_the_Middle.py
@@ -1,33 +1,35 @@
 with open("test1.txt", "r") as file:
     monkey_notes = file.read().replace(" ", "").splitlines()
+monkey_notes.append("")
 
-notes = []
+to_monkey, starting_items, operation, test = [], [], [], []
 for note in monkey_notes:
-    if note:
-        notes.append(note)
-print(notes)
-
-monkey = 0
-for note in notes:
     a = note.find(":")
-    if note[0] == "M":
-        monkey = int(note[6])
+    items = []
+    if note:
+        if note[0] == "M":
+            monkey = int(note[6:len(note)-1])
+        if note[0] == "S":
+            items = note[a+1:].split(",")
+            for i in items:
+                items[items.index(i)] = int(i)
+            starting_items.insert(monkey, items)
+        if note[0] == "O":
+            operation.insert(monkey, note[17:])
+        if note[0] == "T":
+            test.insert(monkey, int(note[16:]))
+        if note[0:3] == "Ift":
+            true = int(note[20])
+            false = int(monkey_notes[monkey_notes.index(note)+1][21])
+            to_monkey.insert(monkey, (true, false))
+print(starting_items)
+print(operation)
+print(test)
+print(to_monkey)
+
+for n in range(20):
+    for items in starting_items:
+        monkey = starting_items.index(monkey)
         print(monkey)
-    if note[0] == "S":
-        item = note[a+1:].split(",")
-        for i in item:
-            old_worry = int(i)
-            if notes[notes.index(note)+1][18:] == "old":
-                value = int(old_worry)
-            else:
-                value = int(notes[notes.index(note)+1][18:])
-            if notes[notes.index(note) + 1][17] == "*":
-                new_worry = old_worry * value
-            if notes[notes.index(note) + 1][17] == "/":
-                new_worry = old_worry / value
-            if notes[notes.index(note) + 1][17] == "+":
-                new_worry = old_worry + value
-            if notes[notes.index(note) + 1][17] == "-":
-                new_worry = old_worry - value
-            worry = new_worry//3
-            print(new_worry, worry)
+        for item in items:
+            if
