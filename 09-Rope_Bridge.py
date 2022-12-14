11c45240721ebe4fae2ed22209ba71fb49694188
ckraasch <corinna@kraasch.de>
2022-12-14 13:12:58 +0100

    Solution for day 9.

--- 09-Rope_Bridge.py
+++ 09-Rope_Bridge.py
@@ -1,60 +1,114 @@
-with open("Motion_Series_test.txt", "r") as file:
+with open("Motion_Series-test2.txt", "r") as file:
     motion_series = file.read().splitlines()
 print(motion_series)
 
 
-def move(a1, b1, x1, y1):
-    if abs(x1 - a1) > 1:
-        if x1 > a1:
-            a1 += 1
-            b1 = y1
-        if x1 < a1:
-            a1 -= 1
-            b1 = y1
-    if abs(y1 - b1) > 1:
-        if y1 > b1:
-            b1 += 1
-            a1 = x1
-        if y1 < b1:
-            b1 -= 1
-            a1 = x1
-    return a1, b1
+def move(a, b, x, y, lst):
+    if abs(x - a) > 1:
+        if x > a:
+            a += 1
+            b = y
+        if x < a:
+            a -= 1
+            b = y
+    if abs(y - b) > 1:
+        if y > b:
+            b += 1
+            a = x
+        if y < b:
+            b -= 1
+            a = x
+    if (a, b) not in lst:
+        lst.append((a, b))
+    return a, b
 
 
 x, y = 0, 0
-position = [(0, 0)]
-a, b = 0, 0
-tail = []
+a1, b1 = 0, 0
+a2, b2 = 0, 0
+a3, b3 = 0, 0
+a4, b4 = 0, 0
+a5, b5 = 0, 0
+a6, b6 = 0, 0
+a7, b7 = 0, 0
+a8, b8 = 0, 0
+a9, b9 = 0, 0
+head = [(0, 0)]
+knot1 = [(0, 0)]
+knot2 = [(0, 0)]
+knot3 = [(0, 0)]
+knot4 = [(0, 0)]
+knot5 = [(0, 0)]
+knot6 = [(0, 0)]
+knot7 = [(0, 0)]
+knot8 = [(0, 0)]
+tail = [(0, 0)]
 for motion in motion_series:
     steps = int(motion[2:])
     if motion[0] in ["R", "L"]:
         if motion[0] == "R":
             for i in range(steps):
                 x += 1
-                position.append((x, y))
-                a, b = move(a, b, x, y)
-                tail.append((a, b))
+                head.append((x, y))
+                a1, b1 = move(a1, b1, x, y, knot1)
+                a2, b2 = move(a2, b2, a1, b1, knot2)
+                a3, b3 = move(a3, b3, a2, b2, knot3)
+                a4, b4 = move(a4, b4, a3, b3, knot4)
+                a5, b5 = move(a5, b5, a4, b4, knot5)
+                a6, b6 = move(a6, b6, a5, b5, knot6)
+                a7, b7 = move(a7, b7, a6, b6, knot7)
+                a8, b8 = move(a8, b8, a7, b7, knot8)
+                a9, b9 = move(a9, b9, a8, b8, tail)
         else:
             for i in range(steps):
                 x -= 1
-                position.append((x, y))
-                a, b = move(a, b, x, y)
-                tail.append((a, b))
+                head.append((x, y))
+                a1, b1 = move(a1, b1, x, y, knot1)
+                a2, b2 = move(a2, b2, a1, b1, knot2)
+                a3, b3 = move(a3, b3, a2, b2, knot3)
+                a4, b4 = move(a4, b4, a3, b3, knot4)
+                a5, b5 = move(a5, b5, a4, b4, knot5)
+                a6, b6 = move(a6, b6, a5, b5, knot6)
+                a7, b7 = move(a7, b7, a6, b6, knot7)
+                a8, b8 = move(a8, b8, a7, b7, knot8)
+                a9, b9 = move(a9, b9, a8, b8, tail)
     else:
         if motion[0] == "U":
             for i in range(steps):
                 y -= 1
-                position.append((x, y))
-                a, b = move(a, b, x, y)
-                tail.append((a, b))
+                head.append((x, y))
+                a1, b1 = move(a1, b1, x, y, knot1)
+                a2, b2 = move(a2, b2, a1, b1, knot2)
+                a3, b3 = move(a3, b3, a2, b2, knot3)
+                a4, b4 = move(a4, b4, a3, b3, knot4)
+                a5, b5 = move(a5, b5, a4, b4, knot5)
+                a6, b6 = move(a6, b6, a5, b5, knot6)
+                a7, b7 = move(a7, b7, a6, b6, knot7)
+                a8, b8 = move(a8, b8, a7, b7, knot8)
+                a9, b9 = move(a9, b9, a8, b8, tail)
         else:
             for i in range(steps):
                 y += 1
-                position.append((x, y))
-                a, b = move(a, b, x, y)
-                tail.append((a, b))
-print(position)
+                head.append((x, y))
+                a1, b1 = move(a1, b1, x, y, knot1)
+                a2, b2 = move(a2, b2, a1, b1, knot2)
+                a3, b3 = move(a3, b3, a2, b2, knot3)
+                a4, b4 = move(a4, b4, a3, b3, knot4)
+                a5, b5 = move(a5, b5, a4, b4, knot5)
+                a6, b6 = move(a6, b6, a5, b5, knot6)
+                a7, b7 = move(a7, b7, a6, b6, knot7)
+                a8, b8 = move(a8, b8, a7, b7, knot8)
+                a9, b9 = move(a9, b9, a8, b8, tail)
+print(head)
+print(knot1)
+print(knot2)
+print(knot3)
+print(knot4)
+print(knot5)
+print(knot6)
+print(knot7)
+print(knot8)
 print(tail)
-print(len(set(tail)))
+print("Part 1:", len(set(knot1)))
+print("Part 2:", len(set(tail)))
 
-# Part 2
